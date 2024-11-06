970252a7ac0a2a373d68b962f184ead00a83938e
emshg <emshg@seas.upenn.edu>
2024-11-05 21:57:09 -0500

    add style for selected nav item

--- front-end/src/components/SideBar/NavItem.tsx
+++ front-end/src/components/SideBar/NavItem.tsx
@@ -10,7 +10,7 @@ interface NavItemProps {
 
 const NavItem: React.FC<NavItemProps> = ({ icon, label }) => {
   return (
-    <div className="flex gap-2.5 mt-5 ml-5 whitespace-nowrap">
+    <div className="flex gap-2.5 my-2.5 ml-5 whitespace-nowrap">
       <img loading="lazy" src={icon} alt="" className="object-contain shrink-0 self-start aspect-square w-[18px]" />
       <div data-layername={label.toLowerCase()}>{label}</div>
     </div>
